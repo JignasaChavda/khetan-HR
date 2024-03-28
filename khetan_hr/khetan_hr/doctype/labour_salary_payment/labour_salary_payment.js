dc01e756b53eee2c03ec4494e5cd483fa5042db2
JignasaChavda <jignasha@sanskartechnolab.com>
2024-03-28 22:46:17 +0530

    Changes in labour salary payment

--- khetan_hr/khetan_hr/doctype/labour_salary_payment/labour_salary_payment.js
+++ khetan_hr/khetan_hr/doctype/labour_salary_payment/labour_salary_payment.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Labour Salary Payment', {
-	// refresh: function(frm) {
-
-	// }
+    from_date: function(frm) {
+        var from_date = frm.doc.from_date;
+        
+        var to_date = frappe.datetime.month_end(from_date);
+        
+        console.log(to_date)
+        frm.set_value('to_date', to_date);
+        frm.refresh_field('to_date');
+        
+    }
 });
