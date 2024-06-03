2c3ff9856f94448e18edb53371e6719b254ae695
JignasaChavda <jignasha@sanskartechnolab.com>
2024-06-03 11:42:09 +0530

    Deleted extra doctype

--- khetan_hr/khetan_hr/doctype/advance_payment/advance_payment.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, jignasha and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Advance Payment', {
-	// refresh: function(frm) {
-
-	// }
-});
